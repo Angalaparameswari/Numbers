8765da903146a09e2bf2500415a6f60c3cacbced
Angalaparameswari <angalaparameswaridevi@gmail.com>
2016-08-17 22:38:03 +0530

    Create StringToInteger.java

---
+++ StringToInteger.java
@@ -0,0 +1,10 @@
+public class StingToInteger {
+
+	public static void main(String[] args) {
+		String s1 = "1234";
+		Integer in = new Integer(s1);
+		int number = in.intValue();
+		System.out.println(number);
+	}
+
+}
